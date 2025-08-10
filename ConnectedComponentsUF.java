098b14bc1323d6400a0c4ba1130a53b55a2d1a29
Krishna Sharma <156648921+Krishna8167@users.noreply.github.com>
2025-08-10 22:54:01 +0530

    Create ConnectedComponentsUF.java
    You are given n nodes labeled from 0 to n-1 and a list of edges.
    Count the number of connected components in the graph using Union-Find.

---
+++ ConnectedComponentsUF.java
@@ -0,0 +1,56 @@
+public class ConnectedComponentsUF {
+
+    static class UnionFind {
+        int[] parent;
+        int[] rank;
+        int count;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++) parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]); // Path compression
+            }
+            return parent[x];
+        }
+
+        void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                count--;
+            }
+        }
+
+        int getCount() {
+            return count;
+        }
+    }
+
+    public static int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            uf.union(edge[0], edge[1]);
+        }
+        return uf.getCount();
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[][] edges = { {0, 1}, {1, 2}, {3, 4} };
+        System.out.println("Number of connected components: " + countComponents(n, edges));
+    }
+}
